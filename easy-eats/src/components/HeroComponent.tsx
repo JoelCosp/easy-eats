a871ad630c7dc27e9635ef9354f33dca326b5c56
JoelVRV <joelc456789@gmail.com>
2025-02-23 02:51:30 +0100

    ---

--- easy-eats/src/components/HeroComponent.tsx
+++ easy-eats/src/components/HeroComponent.tsx
@@ -5,7 +5,8 @@ const HeroComponent = () => {
     <>
         <section>
             <div className='max-w-[1440px] mx-auto font-semibold items-center text-[#1A1A19] px-5'>
-                {/* <div className='grid grid-cols-2'>
+                
+                <div className='hidden md:grid md:grid-cols-2'>
                     <div className='flex flex-col justify-center'>
                         <h2 className='text-[#BC282D] text-2xl font-bold py-4'>"EAT WELL, FEEL GREAT</h2>
                         <h1 className='font-bold text-6xl py-4'>Eat healthy with a new <br /> recipe every day</h1>
@@ -15,12 +16,12 @@ const HeroComponent = () => {
                             <button className='font-bold px-[50px] py-5 rounded-full ml-5 text-xl hover:cursor-pointer'>Explore →</button>
                         </div>
                     </div>
-                    <div className='flex justify-end'>
+                    <div className='justify-end hidden md:flex'>
                         <img src="src/assets/heroPlate.png" alt="Delicious meal" />
                     </div>
-                </div> */}
+                </div>
 
-                <div className='bg-[#1A1A19] max-h-[167px] overflow-hidden flex justify-between my-5 rounded-2xl'>
+                <div className='bg-[#1A1A19] max-h-[167px] sm:hidden overflow-hidden flex justify-between my-5 rounded-2xl'>
                     <div className='p-5 flex flex-col justify-between'>
                         <h1 className='text-white font-bold text-xl'>¡Healthy eat <br /> every day with us!</h1>
                         {/* <p className='text-white'>Discover tasty meals to <br /> keep your days fresh</p> */}
--- easy-eats/src/components/NavComponent.tsx
+++ easy-eats/src/components/NavComponent.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// ICONS
+import { Menu } from "lucide-react";
+
 const NavComponent = () => {
   return (
     <nav>
@@ -13,6 +16,11 @@ const NavComponent = () => {
                 <li className='px-2 py-6 border-b-2 border-transparent hover:border-[#1A1A19] hover: cursor-pointer'>CALENDAR</li>
             </ul>
             <img className='max-w-[40px] max-h-[40px]' src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="" />
+        </div>
+
+        <div className='max-w-[1440px] mx-auto flex sm:hidden justify-between font-semibold items-center text-[#1A1A19] px-5 pt-5'>
+          <Menu size={35}/>
+          <img className='max-w-[35px]' src="https://imgproxy.attic.sh/unsafe/rs:fit:540:540:1:1/t:1:FF00FF:false:false/aHR0cHM6Ly9hdHRp/Yy5zaC9ia3A1a3p5/cGd3YXdlODFhaWts/Y3J6MzhpM2Rm.webp" alt="" />
         </div>
     </nav>
   )
